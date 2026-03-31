070f17ad0db4ceff585071bbdec3942a18dae4fc
Garrett Leber <garrettleber@Garrett-Leber-FJPGQQ9WRM.local>
2026-03-31 14:29:24 -0400

    fix: bug preventing macos builds due to user home dir var

--- shared/workstations/darwin/default.nix
+++ shared/workstations/darwin/default.nix
@@ -1,4 +1,12 @@
 { user, ... }:
 {
   system.primaryUser = user;
+
+  # home-manager's common module sets home.homeDirectory from
+  # config.users.users.<name>.home, which is null on Darwin by default
+  # (unlike NixOS which defaults to /home/<user>). Setting it here
+  # ensures home-manager sees a valid absolute path.
+  users.users.${user} = {
+    home = "/Users/${user}";
+  };
 }
